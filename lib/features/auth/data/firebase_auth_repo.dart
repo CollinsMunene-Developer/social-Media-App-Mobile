3b23fadd91f383cca76b63adcacbb3ffb7a19f75
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2024-10-19 11:58:05 +0000

    "Added Firebase authentication implementation to the app, including plugin registrations and dependencies."

--- lib/features/auth/data/firebase_auth_repo.dart
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:social_app/features/auth/domain/entities/app_user.dart';
 import 'package:social_app/features/auth/domain/repos/auth_repo.dart';
 
 class FirebaseAuthRepo  implements AuthRepo{
+
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 @override
   Future<AppUser?> loginWithEmailPassowrd(String email, String password) {
     // TODO: implement loginWithEmailPassowrd
+    try{
+      //attempt sign in
+      UserCredential userCredential = await firebaseAuth.signInWithEmailAndPassword(email: email, password: password)
+      
+    }
+    catch(e){
+    }
     throw UnimplementedError();
   }
 
